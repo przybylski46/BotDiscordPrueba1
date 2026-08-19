2e44b366890596f69ed9e5108d3c0a24a6bb10ff
przybylski46 <przybylski4637@gmail.com>
2026-08-19 00:41:59 -0600

    Update and rename deploy-commands.js to registrarComandos.js

---
+++ src/registrarComandos.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const comandos = [];
+
+const rutaComandos = path.join(__dirname, 'carpetaComandos');
+const archivosComandos = fs.readdirSync(rutaComandos).filter(archivo => archivo.endsWith('.js'));
+
+for (const archivo of archivosComandos) {
+    const rutaArchivo = path.join(rutaComandos, archivo);
+    const comando = require(rutaArchivo);
+
+    comandos.push(comando.data.toJSON());
+}
+
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+    try {
+        console.log(`Registrando ${comandos.length} comando(s)...`);
+
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: comandos }
+        );
+
+        console.log('Comandos registrados correctamente.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
